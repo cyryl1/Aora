28a695607119010e86547d415f5e904868e0575e
Praise Aribisala <aribisalapraise12@gmail.com>
2025-05-12 10:55:08 +0100

    chore: update expo to version 53.0.9 and add react-native-appwrite dependency
    feat: implement Appwrite client and user authentication functions
    - Added createUser and signIn functions for user management
    - Configured Appwrite client with endpoint, project ID, and database details
    - Integrated avatar generation using Appwrite's avatars service

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -52,7 +52,7 @@ const SignIn = () => {
           />
           <View className="justify-center pt-5 flex-row gap-2">
             <Text className='text-lg text-gray-100 font-pregular'>
-              Don't have account?
+              
             </Text>
             <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>Sign Up</Link>
           </View>
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { images } from '../../constants';
 import FormField from '@components/FormField';
 import CustomButton from '@components/CustomButton';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
+import { createUser } from 'lib/appwrite';
 
 const SignUp = () => {
   const [form, setForm] = useState({
@@ -16,8 +17,24 @@ const SignUp = () => {
 
   const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert('Error', 'Please fill in all fields');
+    }
 
+    setIsSubmitting(true);
+
+    try {
+      const result = await createUser(form.email, form.password, form.username);
+
+      //set to global state
+
+      router.replace('/home')
+    } catch (error) {
+      Alert.alert('Error', (error as Error).message);
+    } finally {
+      setIsSubmitting(false);
+    }
   }
 
   return (
